9b710c2f13fca8370c4ff3c975f3f1f10e993df5
naufalzp <naufalarya0@gmail.com>
2025-06-03 21:17:39 +0700

    feat: Organize routes for Obat and Jadwal Periksa with proper naming conventions

--- routes/dokter.php
+++ routes/dokter.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Dokter\JadwalPeriksaController;
 use App\Http\Controllers\Dokter\ObatController;
 use Illuminate\Support\Facades\Route;
 
@@ -8,12 +9,23 @@ Route::middleware(['auth', 'role:dokter'])->prefix('dokter')->group(function () 
         return view('dokter.dashboard');
     })->name('dokter.dashboard');
 
-    Route::prefix('obat')->group(function () {
-        Route::get('/', [ObatController::class, 'index'])->name('dokter.obat.index');
-        Route::get('/create', [ObatController::class, 'create'])->name('dokter.obat.create');
-        Route::post('/', [ObatController::class, 'store'])->name('dokter.obat.store');
-        Route::get('/{id}/edit', [ObatController::class, 'edit'])->name('dokter.obat.edit');
-        Route::patch('/{id}', [ObatController::class, 'update'])->name('dokter.obat.update');
-        Route::delete('/{id}', [ObatController::class, 'destroy'])->name('dokter.obat.destroy');
+    // Obat routes
+    Route::prefix('obat')->name('dokter.obat.')->group(function () {
+        Route::get('/', [ObatController::class, 'index'])->name('index');
+        Route::get('/create', [ObatController::class, 'create'])->name('create');
+        Route::post('/', [ObatController::class, 'store'])->name('store');
+        Route::get('/{id}/edit', [ObatController::class, 'edit'])->name('edit');
+        Route::put('/{id}', [ObatController::class, 'update'])->name('update');
+        Route::delete('/{id}', [ObatController::class, 'destroy'])->name('destroy');
+    });
+
+    Route::prefix('jadwal-periksa')->name('dokter.jadwal-periksa.')->group(function () {
+        Route::get('/', [JadwalPeriksaController::class, 'index'])->name('index');
+        Route::get('/create', [JadwalPeriksaController::class, 'create'])->name('create');
+        Route::post('/', [JadwalPeriksaController::class, 'store'])->name('store');
+        Route::get('/{id}/edit', [JadwalPeriksaController::class, 'edit'])->name('edit');
+        Route::put('/{id}', [JadwalPeriksaController::class, 'update'])->name('update');
+        Route::patch('/{id}', [JadwalPeriksaController::class, 'updateStatus'])->name('updateStatus');
+        Route::delete('/{id}', [JadwalPeriksaController::class, 'destroy'])->name('destroy');
     });
 });

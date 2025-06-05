a0590edd4eb8853a992d367818e4b7ca28cdcfd2
naufalzp <naufalarya0@gmail.com>
2025-06-05 14:23:12 +0700

    feat: Add Jadwal Periksa seeder to DatabaseSeeder for schedule data population

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,6 +16,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             ObatSeeder::class,
             DokterSeeder::class,
+            JadwalPeriksaSeeder::class
         ]);
     }
 }

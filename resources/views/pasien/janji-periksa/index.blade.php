0c1c6447a3857699567775c76de4da745a94f72f
naufalzp <naufalarya0@gmail.com>
2025-06-17 15:18:41 +0700

    fix: Update poli display to show poli name instead of object in appointment views

--- resources/views/pasien/janji-periksa/index.blade.php
+++ resources/views/pasien/janji-periksa/index.blade.php
@@ -33,7 +33,7 @@
                   <option value="" disabled hidden selected>Pilih Dokter</option>\
                   @foreach ($dokters as $dokter)
                     @foreach ($dokter->jadwalPeriksas as $jadwalPeriksa)
-                      <option value="{{ $jadwalPeriksa->id }}">{{ $dokter->nama }} - Spesialis {{ $dokter->poli }}|
+                      <option value="{{ $jadwalPeriksa->id }}">{{ $dokter->nama }} - Spesialis {{ $dokter->poli->nama }}|
                         {{ $jadwalPeriksa->hari }}, {{ $jadwalPeriksa->jam_mulai }} - {{ $jadwalPeriksa->jam_selesai }}</option>
                     @endforeach
                   @endforeach

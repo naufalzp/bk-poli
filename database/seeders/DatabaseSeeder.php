5af847edfef15485a37e341b139489b841eac09d
naufalzp <naufalarya0@gmail.com>
2025-06-17 15:28:35 +0700

    fix: Add PoliSeeder to the database seeder for complete data initialization

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,7 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
+            PoliSeeder::class,
             ObatSeeder::class,
             DokterSeeder::class,
             JadwalPeriksaSeeder::class
